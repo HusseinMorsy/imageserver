73d60f63aee52b09786b1a4c3f90832a07006b77
Hussein Morsy <hussein@morsy.de>
2009-10-19 22:24:31 +0200

    initial import

---
+++ config.ru
@@ -0,0 +1,12 @@
+require 'rubygems'
+require 'sinatra'
+require 'imageserver'
+
+root_dir = File.dirname(__FILE__)
+
+set :environment, ENV['RACK_ENV'].to_sym
+set :root,        root_dir
+set :app_file,    File.join(root_dir, 'imageserver.rb')
+disable :run
+
+run Sinatra::Application
---
+++ imageserver.rb
@@ -0,0 +1,70 @@
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+require 'dm-core'
+require 'do_sqlite3'
+
+configure :development do
+  DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/imageserver.sqlite3")
+end
+
+configure :production do
+  DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/imageserver.sqlite3")
+end
+
+
+class Image
+  include DataMapper::Resource
+  
+  property :name, String, :key => true
+  property :content_type, String
+  property :size, String
+  
+  def filename
+    name + ".jpg"
+  end
+  
+  def handle_upload( file )
+    self.content_type = file[:type]
+    self.size = File.size(file[:tempfile])
+    path = File.join(Dir.pwd, "/public", self.filename)
+    File.open(path, "wb") do |f|
+      f.write(file[:tempfile].read)
+    end
+  end
+end
+
+configure :development do
+  # Create or upgrade all tables at once, like magic
+  DataMapper.auto_upgrade!
+end
+
+# set utf-8 for outgoing
+before do
+  headers "Content-Type" => "text/html; charset=utf-8"
+end
+
+get '/' do
+  @images = Image.all(:order => [:name.asc])
+  haml :list
+end
+
+get '/new' do
+   haml :new
+end
+
+get '/hello' do
+  "hello"
+end
+
+post '/new' do
+  name = params[:name]
+
+  image = Image.new(:name => name)
+  image.handle_upload(params[:image])
+  if image.save
+    redirect "/"
+  else
+    redirect "/new"
+  end
+end
