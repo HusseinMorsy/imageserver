f78bad6668a5765fc36a89106f29b9cc99165855
Hussein Morsy <hussein@morsy.de>
2012-01-09 21:44:01 +0100

    implemented delete image

--- imageserver.rb
+++ imageserver.rb
@@ -4,6 +4,11 @@ require 'haml'
 require 'dm-core'
 require 'do_sqlite3'
 
+use Rack::Auth::Basic do |username, password|
+  [username, password] == ['kieferserver', 'bilder']
+end
+
+
 configure :development do
   DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/imageserver.sqlite3")
 end
@@ -15,15 +20,15 @@ end
 
 class Image
   include DataMapper::Resource
-  
+
   property :name, String, :key => true
   property :content_type, String
   property :size, String
-  
+
   def filename
     name + ".jpg"
   end
-  
+
   def handle_upload( file )
     self.content_type = file[:type]
     self.size = File.size(file[:tempfile])
@@ -31,6 +36,11 @@ class Image
     File.open(path, "wb") do |f|
       f.write(file[:tempfile].read)
     end
+  end
+
+  def delete
+    File.delete File.join(Dir.pwd, "/public",self.filename)
+    self.destroy
   end
 end
 
@@ -67,4 +77,11 @@ post '/new' do
   else
     redirect "/new"
   end
+end
+
+get '/destroy' do
+  filename = params[:filename]
+  image = Image.first(:name => params[:name])
+  image.delete
+  redirect "/"
 end
